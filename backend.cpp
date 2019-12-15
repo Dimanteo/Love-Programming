1782e7ca67dd75e88907e6cb48f5bfa8a8174c3e
Dimanteo <Kolobae.DA@phystech.edu>
2019-12-15 14:13:19 +0300

    Added asm code generation

--- backend.cpp
+++ backend.cpp
@@ -13,6 +13,7 @@ const int NOT_SET = -2;
 int RAM_POINTER = 0;
 int GLOBAL = -1;
 const char BASE_REG[] = "bx";
+const char ADRESS_REG[] = "ax";
 
 //Переменные
 struct Variable {
@@ -38,7 +39,10 @@ void Tree<Node>::valuePrint(FILE *file) {
             fprintf(file, "%lf", value.num);
             break;
         case Node::VARIABLE_TYPE:
-            fprintf(file, "%s RAM[%d] lvl[%d]", VARIABLES[value.code].ID, VARIABLES[value.code].adress, VARIABLES[value.code].level);
+            fprintf(file, "%s RAM[%d] lvl[%s]",
+                    VARIABLES[value.code].ID,
+                    VARIABLES[value.code].adress,
+                    (VARIABLES[value.code].level == GLOBAL) ? "GLOBAL" : FUNCTIONS[VARIABLES[value.code].level].ID);
             break;
         case Node::OPERATION_TYPE:
             fprintf(file, "%s varc[%d]", FUNCTIONS[value.code].ID, FUNCTIONS[value.code].varc);
@@ -48,10 +52,14 @@ void Tree<Node>::valuePrint(FILE *file) {
     }
 }
 
+template<>
+void Tree<Node>::valueDestruct() {
+    value.~Node();
+}
 
-Node makeNode(char *value, Tree<Node> *parent);
+Node makeNode(char *value, Tree<Node> *node);
 
-Tree<Node>* parseToAST(char** ast, Tree<Node>* parent);
+Tree<Node>* parseToAST(char** ast, Tree<Node>* node);
 
 int makeSTDNode(char* value);
 
@@ -69,8 +77,8 @@ int main() {
     size_t size = 0;
     char* data = read_file_to_buffer_alloc("../Maksim.txt", "rb", &size);
     char* data_copy = data;
-    Tree<Node>* tree = parseToAST(&data_copy, nullptr);
-    printf("data: %s", data_copy);
+    Tree<Node>* tree = new Tree<Node>(Node());
+    parseToAST(&data_copy, tree);
     assert(tree);
     free(data);
 
@@ -79,9 +87,10 @@ int main() {
     tree->inorder(sequence);
     tree->treeDump(dump, OK_STATE, "main", VERIFY_CONTEXT, sequence);
     tree->graphDump("AST.png", sequence);
-
     free(sequence);
     fclose(dump);
+
+    makeAssembler("E:/C_Progs/Processor Laba/asm_stdin.txt", tree);
 
     for (int i = 0; i < FUNCTION_COUNT; ++i) {
         free(FUNCTIONS[i].ID);
@@ -89,13 +98,21 @@ int main() {
     for (int i = 0; i < VARIABLE_COUNT; ++i) {
         free(VARIABLES[i].ID);
     }
+    delete (tree);
 }
 
 void makeAssembler(const char *filename, Tree<Node> *tree) {
     FILE* assembler = fopen(filename, "wb");
-    fprintf(assembler, "\tpush %d\n"
+    time_t now = time(nullptr);
+    fprintf(assembler, ";<--This is auto-generated file-->\n"
+                       ";Generated %s\n"
+                       "\tpush %d\n"
                        "\tpop %s\n"
-                       "jmp $main", RAM_POINTER, BASE_REG);//TODO makeAsm. END point, and pretty header
+                       "\tcall $main\n"
+                       "end\n",
+                       ctime(&now), RAM_POINTER, BASE_REG);
+    translate(assembler, tree);
+    fclose(assembler);
 }
 
 int makeSTDNode(char *value) {
@@ -108,7 +125,7 @@ int makeSTDNode(char *value) {
     return -1;
 }
 
-Node makeNode(char *value, Tree<Node> *parent) {
+Node makeNode(char *value, Tree<Node> *node) {
     assert(value);
     int code = 0;
     if ((code = makeSTDNode(value)) != -1) {
@@ -128,7 +145,7 @@ Node makeNode(char *value, Tree<Node> *parent) {
     }
 
     //if value is Variable
-    if (parent->getParent()->getValue().type == Node::SPECIAL_SYMBOLS && parent->getParent()->getValue().code == DOT_COMA) {
+    if (node->getParent()->getValue().type == Node::SPECIAL_SYMBOLS && node->getParent()->getValue().code == DOT_COMA) {
         for (int i = 0; i < VARIABLE_COUNT; ++i) {
             if (strcmp(value, VARIABLES[i].ID) == 0 && VARIABLES[i].level == GLOBAL)
                 return Node(Node::VARIABLE_TYPE, i);
@@ -136,7 +153,7 @@ Node makeNode(char *value, Tree<Node> *parent) {
         VARIABLES[VARIABLE_COUNT] = {strdup(value), RAM_POINTER++, GLOBAL};
         return Node(Node::VARIABLE_TYPE, VARIABLE_COUNT++);
     } else {
-        Tree<Node>* function = parent;
+        Tree<Node>* function = node->getParent();
         while (function->getValue().type != Node::OPERATION_TYPE) {
             function = function->getParent();
         }
@@ -154,14 +171,14 @@ Node makeNode(char *value, Tree<Node> *parent) {
     }
 }
 
-Tree<Node>* parseToAST(char** ast, Tree<Node>* parent) {
+Tree<Node>* parseToAST(char** ast, Tree<Node>* node) {
     assert(ast);
     char buffer[MAX_STRING_SIZE] = "";
     int length = 0;
     sscanf(*ast, "{%[^{}@]%n", buffer, &length);
-    printf("%s\n", buffer);
     char* value = strdup(buffer);
-    Tree<Node>* node = new Tree<Node>(makeNode(value, parent));
+    assert(node);
+    node->setValue(makeNode(value, node));
 
     //{v{}{}} {v@@} {v{}@} {v@{}}
     (*ast) += length;
@@ -170,7 +187,8 @@ Tree<Node>* parseToAST(char** ast, Tree<Node>* parent) {
     }
     switch (**ast) {
         case '{':
-            node->connectSubtree(LEFT_CHILD, parseToAST(ast, node));
+            node->growChild(LEFT_CHILD, Node());
+            parseToAST(ast, node->getChild(LEFT_CHILD));
             break;
         case '}':
             (*ast)++;
@@ -183,8 +201,10 @@ Tree<Node>* parseToAST(char** ast, Tree<Node>* parent) {
     while (**ast != '{' && **ast != '}') {
         (*ast)++;
     }
-    if (**ast == '{')
-        node->connectSubtree(RIGHT_CHILD, parseToAST(ast, node));
+    if (**ast == '{') {
+        node->growChild(RIGHT_CHILD, Node());
+        parseToAST(ast, node->getChild(RIGHT_CHILD));
+    }
     (*ast)++;
     free(value);
     return node;
@@ -193,7 +213,7 @@ Tree<Node>* parseToAST(char** ast, Tree<Node>* parent) {
 void translate(FILE *assembler, Tree<Node>* node) {
     switch (node->getValue().type) {
         case Node::NUMBER_TYPE:
-            fprintf(assembler, "push %lf\n", node->getValue().num);
+            fprintf(assembler, "\tpush %lf\n", node->getValue().num);
             break;
         case Node::VARIABLE_TYPE:
             variableHandler(assembler, node);
@@ -224,9 +244,15 @@ void specialSymbolHandler(FILE *assembler, Tree<Node> *node) {
     switch (node->getValue().code) {
         case RET:
             if (!node->childIsEmpty(LEFT_CHILD)) {
+                fprintf(assembler, ";<--Return values-->\n");
                 translate(assembler, node->getChild(LEFT_CHILD));
             }
-            fprintf(assembler, "ret\n");
+            fprintf(assembler, ";Swap return adress and values\n"
+                               "\tpop cx\n"
+                               "\tpop dx\n"
+                               "\tpush cx\n"
+                               "\tpush dx\n"
+                               "ret\n");
             break;
         case DOT_COMA:
         case OP:
@@ -236,6 +262,7 @@ void specialSymbolHandler(FILE *assembler, Tree<Node> *node) {
                 translate(assembler, node->getChild(RIGHT_CHILD));
             break;
         case PUT:
+            translate(assembler, node->getChild(LEFT_CHILD));
             fprintf(assembler, "\tout\n");
             break;
         case GET:
@@ -252,7 +279,8 @@ void specialSymbolHandler(FILE *assembler, Tree<Node> *node) {
             translate(assembler, node->getChild(LEFT_CHILD));
             fprintf(assembler, " if_%d\n", if_number);
             if (it_is(RIGHT_CHILD, Node::SPECIAL_SYMBOLS, IF_ELSE)) {
-                translate(assembler, node->getChild(RIGHT_CHILD)->getChild(RIGHT_CHILD));
+                if (!node->getChild(RIGHT_CHILD)->childIsEmpty(RIGHT_CHILD))
+                    translate(assembler, node->getChild(RIGHT_CHILD)->getChild(RIGHT_CHILD));
                 fprintf(assembler, "\tjmp if_%d_end\nif_%d:\n", if_number, if_number);
                 translate(assembler, node->getChild(RIGHT_CHILD)->getChild(LEFT_CHILD));
             } else {
@@ -264,17 +292,18 @@ void specialSymbolHandler(FILE *assembler, Tree<Node> *node) {
         }
         case WHILE: {
             int cycle_number = CYCLE_COUNTER++;
-            fprintf(assembler, "cycle_%d:", cycle_number);
+            fprintf(assembler, "cycle_%d:\n", cycle_number);
             translate(assembler, node->getChild(RIGHT_CHILD));
             translate(assembler, node->getChild(LEFT_CHILD));
             fprintf(assembler, " cycle_%d\n", cycle_number);
             break;
         }
-        postorder(MORE, "\tjb\n")
-        postorder(LESS, "\tja\n")
-        postorder(MORE_EQUAL, "\tjbe\n")
-        postorder(LESS_EQUAL, "\tjae\n")
-        postorder(EQUAL, "\tje\n")
+        postorder(MORE, "\tjb")
+        postorder(LESS, "\tja")
+        postorder(MORE_EQUAL, "\tjbe")
+        postorder(LESS_EQUAL, "\tjae")
+        postorder(EQUAL, "\tje")
+        postorder(NOT_EQUAL, "\tjne")
         postorder(ADDITION, "\tadd\n")
         postorder(MINUS, "\tsub\n")
         postorder(DIVIDE, "\tdiv\n")
@@ -286,21 +315,29 @@ void specialSymbolHandler(FILE *assembler, Tree<Node> *node) {
 
 void functionHandler(FILE *assembler, Tree<Node> *node) {
     Tree<Node>* node_copy = node;
-    if (node->getParent()->getParent()->getValue().type == Node::SPECIAL_SYMBOLS
-    && node->getParent()->getParent()->getValue().code == DOT_COMA) {
+    if (node->getParent()->getValue().type == Node::SPECIAL_SYMBOLS
+    && node->getParent()->getValue().code == DOT_COMA) {
         fprintf(assembler, "%s:\n"
+                           "\tpop %s; Save call adress to register\n"
+                           ";<--RAM adress setup-->\n"
                            "\tpush %d\n"
                            "\tpush %s\n"
                            "\tadd\n"
-                           "\tpop %s\n",
-                           FUNCTIONS[node->getValue().code].ID, FUNCTIONS[node->getValue().code].varc, BASE_REG, BASE_REG);
+                           "\tpop %s\n"
+                           ";<--Parameters initialization-->\n",
+                           FUNCTIONS[node->getValue().code].ID, ADRESS_REG, FUNCTIONS[node->getValue().code].varc, BASE_REG, BASE_REG);
         while (!node_copy->childIsEmpty(LEFT_CHILD)) {
             assert(it_is(LEFT_CHILD, Node::SPECIAL_SYMBOLS, COMA_PARAMETER));
             node_copy = node_copy->getChild(LEFT_CHILD);
-            assert(node_copy->getChild(RIGHT_CHILD)->getValue().code == Node::VARIABLE_TYPE);
+            assert(node_copy->getChild(RIGHT_CHILD)->getValue().type == Node::VARIABLE_TYPE);
             fprintf(assembler, "\tpop [%s + %d]\n", BASE_REG, VARIABLES[node_copy->getChild(RIGHT_CHILD)->getValue().code].adress);
         }
+        fprintf(assembler, "\tpush %s; Push call adress to stack\n"
+                           ";<--Body-->\n",
+                           ADRESS_REG);
         translate(assembler, node->getChild(RIGHT_CHILD));
+        //if (strcmp(FUNCTIONS[node->getValue().code].ID, "$main") == 0)
+        fprintf(assembler, "ret\n");
     } else {
         while (!node_copy->childIsEmpty(LEFT_CHILD)) {
             assert(it_is(LEFT_CHILD, Node::SPECIAL_SYMBOLS, COMA_PARAMETER));
@@ -308,6 +345,7 @@ void functionHandler(FILE *assembler, Tree<Node> *node) {
             translate(assembler, node_copy->getChild(RIGHT_CHILD));
         }
         fprintf(assembler, "\tcall %s\n"
+                           ";<--RAM adress free-->\n"
                            "\tpush %s\n"
                            "\tpush %d\n"
                            "\tsub\n"
@@ -321,15 +359,15 @@ void variableHandler(FILE *assembler, Tree<Node> *node) {
     if (node->getParent()->getValue().type == Node::SPECIAL_SYMBOLS
     && (node->getParent()->getValue().code == ASSIGN || node->getParent()->getValue().code == GET)) {
         if (var->level == GLOBAL) {
-            fprintf(assembler, "pop [%d]", var->adress);
+            fprintf(assembler, "\tpop [%d]\n", var->adress);
         } else {
-            fprintf(assembler, "pop [%s + %d]", BASE_REG, var->adress);
+            fprintf(assembler, "\tpop [%s + %d]\n", BASE_REG, var->adress);
         }
     } else {
         if (var->level == GLOBAL) {
-            fprintf(assembler, "push [%d]", var->adress);
+            fprintf(assembler, "\tpush [%d]\n", var->adress);
         } else {
-            fprintf(assembler, "push [%s + %d]", BASE_REG, var->adress);
+            fprintf(assembler, "\tpush [%s + %d]\n", BASE_REG, var->adress);
         }
     }
 }
